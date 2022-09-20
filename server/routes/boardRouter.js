098bde0e7a5517c054ac0433557c7be225980d28
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-20 22:05:19 +0300

    Apply authMiddleware

--- server/routes/boardRouter.js
+++ server/routes/boardRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express');
 const router = new Router();
-const boardController = require('../controllers/boardController')
+const boardController = require('../controllers/boardController');
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/', boardController.create) // Create board
-router.get('/', boardController.getAll) // Get all boards, belongs to user
-router.get('/:id', boardController.getOne) // Get one board by id
-router.delete('/', boardController.delete) // Delete board by id
+router.post('/', authMiddleware, boardController.create) // Create board
+router.get('/', authMiddleware, boardController.getAll) // Get all boards, belongs to user
+router.get('/:id', authMiddleware, boardController.getOne) // Get one board by id
+router.delete('/', authMiddleware, boardController.delete) // Delete board by id
 
 module.exports = router

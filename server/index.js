0d88dc601699986b1858d427bab9f457af6d8ecd
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-20 23:02:11 +0300

    Apply error handling middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const models = require('./models/models')
 const sequelize = require('./db');
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5050
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/api', router);
+
+app.use(errorHandler)
 
 const start = async () => {
     try {

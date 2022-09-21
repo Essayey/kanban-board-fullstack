9d2e216442abad96e128e309dd8e8978cf1dae5b
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-21 20:13:11 +0300

    Add card controller create method

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,9 +1,28 @@
-class CardController {
-    async create(req, res) {
+const ApiError = require("../error/ApiError");
+const { Card, List, UserBoard } = require("../models/models");
 
+class CardController {
+    async create(req, res, next) {
+        const { listId, title } = req.body;
+        const card = await Card.create({ listId, title, order: await Card.count({ where: { listId } }) });
+
+        const list = await List.findOne({ where: { id: listId }, attributes: ['boardId'] });
+
+        const hasAccess = await UserBoard.findOne({
+            where: {
+                userId: req.user.id,
+                boardId: list.boardId
+            }
+        });
+
+        if (!hasAccess) {
+            return next(ApiError.forbidden('У вас нет доступа к этой доске'));
+        }
+
+        res.json(card);
     }
     async getOne(req, res) {
-        res.json({ message: 'Hello, this is working' })
+
     }
     async update(req, res) {
 

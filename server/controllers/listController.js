83d9e5bacaa46ddebbed4d37d8f18391125f6e8b
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-21 20:13:21 +0300

    Add list controller create method

--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -1,6 +1,17 @@
-class ListController {
-    async create(req, res) {
+const ApiError = require('../error/ApiError');
+const { List, UserBoard } = require('../models/models')
 
+class ListController {
+    async create(req, res, next) {
+        const { boardId, title } = req.body;
+
+        const hasAccess = await UserBoard.findOne({ where: { userId: req.user.id, boardId } });
+        if (!hasAccess) {
+            return next(ApiError.forbidden('У вас нет доступа к этой доске'));
+        }
+
+        const list = await List.create({ title, boardId, order: await List.count({ where: { boardId } }) });
+        res.json({ list });
     }
     async delete(req, res) {
 

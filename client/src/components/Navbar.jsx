393f71037bf274229ef3b1f6723abef9f252ccff
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-22 20:05:37 +0300

    Set up

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar

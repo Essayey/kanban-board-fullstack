9c32a98f6e0dcb481d01ea4eb78b7ac3af004ee8
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-22 20:50:45 +0300

    Create Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../Styles/Navbar.css'
+import Button from './UI/Button/Button'
 
 const Navbar = () => {
+    const [isAuth, setIsAuth] = useState(false);
+    const navigate = useNavigate();
+
+
     return (
         <div className='Navbar'>
-            Navbar
+            <div className="Navbar__inner">
+                <div onClick={() => navigate('/')}>LOGO</div>
+                <div className="Navbar__links">
+                    {!isAuth
+                        ? <Fragment>
+                            <Button onClick={() => { setIsAuth(true) }}>Войти</Button>
+                            <Button onClick={() => navigate('/registration')}>Регистрация</Button>
+                        </Fragment>
+                        : <Fragment>
+                            <Button onClick={() => navigate('/boards')}>Мои доски</Button>
+                            <Button onClick={() => { setIsAuth(false) }}>Выйти</Button>
+                        </Fragment>
+                    }
+                </div>
+            </div>
         </div>
     )
 }

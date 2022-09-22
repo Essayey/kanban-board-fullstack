44032c6fa2566897edfb32ac6904bd43e050e7f0
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-22 16:30:16 +0300

    Add update routes

--- server/routes/cardRouter.js
+++ server/routes/cardRouter.js
@@ -5,7 +5,8 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, cardController.create) // Create card
 router.get('/:id', authMiddleware, cardController.getOne) // Get card by id
-router.put('/', authMiddleware, cardController.update) // Update card by id
+router.put('/title', authMiddleware, cardController.updateTitle) // Update card by id
+router.put('/description', authMiddleware, cardController.updateDescription) // Update card by id
 router.delete('/', authMiddleware, cardController.delete) // Delete card by id
 
 module.exports = router
--- server/routes/listRouter.js
+++ server/routes/listRouter.js
@@ -4,6 +4,7 @@ const listController = require('../controllers/listController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/', authMiddleware, listController.create) // Create list
+router.put('/', authMiddleware, listController.updateTitle) // Update title of the list
 router.delete('/', authMiddleware, listController.delete) // Delete list by id
 
 module.exports = router

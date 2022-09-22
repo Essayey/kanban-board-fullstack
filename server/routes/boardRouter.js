9cfb3d58ed71f2ef5ad7bd6a10cc2f19e6df7645
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-22 16:37:03 +0300

    Add update routes

--- server/routes/boardRouter.js
+++ server/routes/boardRouter.js
@@ -6,6 +6,8 @@ const authMiddleware = require('../middleware/authMiddleware')
 router.post('/', authMiddleware, boardController.create) // Create board
 router.get('/', authMiddleware, boardController.getAll) // Get all boards, belongs to user
 router.get('/:id', authMiddleware, boardController.getOne) // Get one board by id
+router.put('/background', authMiddleware, boardController.updateBackground) // Update background
+router.put('/name', authMiddleware, boardController.updateName) // Update name
 router.delete('/', authMiddleware, boardController.delete) // Delete board by id
 
 module.exports = router

2208aff163805819ea02f1825f4997b5c5cfb7ca
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-23 12:56:16 +0300

    Create sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../Styles/Sidebar.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { BOARDS_ROUTE } from '../utils/consts'
 
 const Sidebar = () => {
+    const boards = [{ name: "board1", id: '1' },
+    { name: "board2", id: '2' },
+    { name: "board3", id: '3' },
+    { name: "board1", id: '1' },
+    { name: "board2", id: '2' },
+    { name: "board3", id: '3' },]
+
+    const navigate = useNavigate();
+
     return (
-        <div>Sidebar</div>
+        <div className="Sidebar">
+            <h2 style={{ paddingLeft: 5 }}>Мои доски</h2>
+            <div className="Sidebar__list">
+                {boards.map(board =>
+                    <div className={'Sidebar__link'} onClick={() => navigate(BOARDS_ROUTE + '/' + board.id)}>{board.name}</div>
+                )}
+            </div>
+        </div>
     )
 }
 

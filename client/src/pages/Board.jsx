9c8854d0f82dd74a43e7f4d046dad428eb8d3c7e
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-23 13:35:29 +0300

    Board page

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../Styles/Board.css'
+import Sidebar from '../components/Sidebar'
+import List from '../components/List'
 
 const Board = () => {
+    const lists = [{ title: 'list1' },
+    { title: 'list2' },
+    { title: 'list3' }]
+
     return (
-        <div>Board</div>
+        <div className='Board'>
+            <Sidebar />
+            <div className="Board__inner">
+                <div className="Board__lists">
+                    {lists.map(list => <List title={list.title} />)}
+
+                </div>
+            </div>
+        </div>
     )
 }
 

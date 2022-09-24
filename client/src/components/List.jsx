74772bbc2d2a47c474193c0d9b529d0c3d4fe1a3
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-24 18:33:56 +0300

    Update List.jsx

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,29 +1,75 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import '../Styles/List.css'
 import Card from './Card';
 
 import Textarea from './UI/Textarea/Textarea';
+import Input from './UI/Input/Input'
 import Button from './UI/Button/Button'
+
+import { useHide } from '../hooks/useHide'
+import { submitOnEnter } from '../utils/utils';
 
 const List = ({ title }) => {
     const cards = ['card1', 'card2', 'card3', 'card4'];
+
+    // Edit title
+    const [titleEditing, setTitleEditing] = useState(false);
+    const [listTitle, setListTitle] = useState(title);
+    const inputRef = useRef();
+    const closeTitleEditing = () => {
+        setTitleEditing(false);
+        setListTitle(title)
+    }
+    useHide(closeTitleEditing, inputRef)
+
+    const editTitle = e => {
+        e.preventDefault();
+        // Request //
+        closeTitleEditing();
+    }
+
+    // Add card
     const [cardAdding, setCardAdding] = useState(false);
     const [cardName, setCardName] = useState('');
+    const formRef = useRef();
 
     const closeAddform = () => {
         setCardAdding(false);
         setCardName('');
     }
+    useHide(closeAddform, formRef);
+
+    const addCard = e => {
+        e.preventDefault();
+        // Request //
+        closeAddform();
+    }
 
     return (
         <div className="List">
             <div className="List__inner">
-                <h3>{title}</h3>
+                {titleEditing
+                    ? <form ref={inputRef} onSubmit={e => editTitle(e)}>
+                        <Input
+                            value={listTitle}
+                            onChange={e => setListTitle(e.target.value)}
+                            autoFocus
+                            style={{ height: 24 }}
+                        />
+                    </form>
+                    : <h3 onClick={() => setTitleEditing(true)}>{title}</h3>
+                }
+
                 <div className="List__cards">
                     {cards.map(card => <Card title={card} key={card} />)}
                     {cardAdding &&
-                        <form onSubmit={e => { e.preventDefault(e) }}>
-                            <Textarea value={cardName} onChange={e => setCardName(e.target.value)} />
+                        <form ref={formRef} onSubmit={e => addCard(e)}>
+                            <Textarea
+                                autoFocus
+                                onKeyPress={e => submitOnEnter(e, formRef)}
+                                value={cardName}
+                                onChange={e => setCardName(e.target.value)}
+                            />
                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                                 <Button style={{ padding: 7, marginTop: 7 }} type='submit'>Add card</Button>
                                 <Button onClick={closeAddform} style={{ padding: 7, marginTop: 7 }}>Закрыть</Button>
@@ -37,7 +83,6 @@ const List = ({ title }) => {
                         Add card +
                     </button>
                 }
-
             </div>
         </div>
     )

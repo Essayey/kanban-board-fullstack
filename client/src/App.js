dfa29cd2c4c5a65abe3cca3c8a98bb9b6c9bb3b6
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-25 21:22:13 +0300

    Add auth cheking logic to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,30 @@ import { BrowserRouter } from 'react-router-dom';
 import './Styles/App.css';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { useContext, useEffect, useState } from 'react';
+import { check } from './http/userAPI'
+import { observer } from 'mobx-react-lite';
+import { Context } from '.';
 
-function App() {
+const App = observer(() => {
+    const { user } = useContext(Context);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        check().then(data => {
+            user.setUser(data);
+            user.setIsAuth(true);
+        }).finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return;
+
     return (
         <BrowserRouter>
             <Navbar />
             <AppRouter />
         </BrowserRouter>
     );
-}
+})
 
 export default App;

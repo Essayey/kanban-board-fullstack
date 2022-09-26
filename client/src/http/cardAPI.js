928dbd0ac272ca149515d124cd90405eb2ccd930
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-26 17:45:10 +0300

    Create cardAPI.js

---
+++ client/src/http/cardAPI.js
@@ -0,0 +1,16 @@
+import { $authHost } from "."
+
+class CardApi {
+    create = async (listId, title) => {
+        const data = $authHost.post('/api/card', { listId, title }).then(res => res.data);
+        return data;
+    }
+}
+export const cardApi = new CardApi()
+
+// API
+// route '/' Create card; const { listId, title } = req.body; POST
+// route '/:id' GET
+// route '/title' PUT
+// route '/description' PUT
+// route '/' DELETE

4fb2c32920d91290733e9e92138e97c890f55991
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-26 17:05:36 +0300

    Apply boardApi in Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Styles/Sidebar.css'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { BOARDS_ROUTE } from '../utils/consts'
+import { boardApi } from '../http/boardAPI'
 
 const Sidebar = () => {
-    const boards = [{ name: "board1", id: '1' },
-    { name: "board2", id: '2' },
-    { name: "board3", id: '3' },
-    { name: "board1", id: '4' },
-    { name: "board2", id: '5' },
-    { name: "board3", id: '6' },]
+    const [boards, setBoards] = useState([]);
+    const { id } = useParams();
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        boardApi.getAll().then(data => setBoards(data.boards));
+    }, [])
+
 
     return (
         <div className="Sidebar">
             <h2 style={{ paddingLeft: 5 }}>Мои доски</h2>
             <div className="Sidebar__list">
                 {boards.map(board =>
-                    <div key={board.id} className={'Sidebar__link'} onClick={() => navigate(BOARDS_ROUTE + '/' + board.id)}>{board.name}</div>
+                    <div
+                        key={board.id}
+                        className={id == board.id ? 'Sidebar__link Sidebar__link-active' : 'Sidebar__link'}
+                        onClick={() => navigate(BOARDS_ROUTE + '/' + board.id)}
+
+                    >
+                        {board.name}
+                    </div>
                 )}
             </div>
         </div>

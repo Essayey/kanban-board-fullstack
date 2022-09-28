796ca15d74e095b86a3653dc09504e5e5d6ca5a2
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-28 20:56:16 +0300

    Add get card method in CardApi

--- client/src/http/cardAPI.js
+++ client/src/http/cardAPI.js
@@ -5,6 +5,10 @@ class CardApi {
         const data = $authHost.post('/api/card', { listId, title }).then(res => res.data);
         return data;
     }
+    get = async (id) => {
+        const data = $authHost.get('/api/card/' + id).then(res => res.data);
+        return data;
+    }
 }
 export const cardApi = new CardApi()
 

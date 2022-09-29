8639a736b1c35ee0c7e53b536061e9cc750a9782
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-29 20:38:14 +0300

    Add height property to modal component

--- client/src/components/UI/Modal/Modal.jsx
+++ client/src/components/UI/Modal/Modal.jsx
@@ -3,17 +3,17 @@ import styles from './Modal.module.css'
 import { useHide } from '../../../hooks/index'
 import CloseButton from '../CloseButton/CloseButton'
 
-const Modal = ({ children, onHide, shouldHide }) => {
+const Modal = ({ children, onHide, shouldHide, height }) => {
     const windowRef = useRef();
     useHide(onHide && shouldHide ? onHide : () => { }, windowRef);
 
     return (
         <div className={styles.Modal}>
             <CloseButton style={{ position: 'absolute', top: 5, right: 5 }} onClick={onHide} />
-            <div ref={windowRef} className={styles.Modal__window}>
+            <div ref={windowRef} className={styles.Modal__window} style={{ minHeight: height }}>
                 {children}
             </div>
-        </div>
+        </div >
     )
 }
 

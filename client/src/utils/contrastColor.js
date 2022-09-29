e41ef5f697635efc65e22a4b9d188a673ca78653
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-29 09:35:44 +0300

    Create contrast color util

---
+++ client/src/utils/contrastColor.js
@@ -0,0 +1,7 @@
+const ContrastColor = require('contrast-color');
+
+export const cc = new ContrastColor({
+    fgDarkColor: "#3e3f42",
+    fgLightColor: "#ffffff",
+    defaultColor: "#3e3f42"
+});

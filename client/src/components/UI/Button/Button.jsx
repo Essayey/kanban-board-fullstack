5207f2ebc9d0e5e9fa06fa5c2a5e2836e901998e
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-01 22:10:33 +0300

    Add variants to Button UI component

--- client/src/components/UI/Button/Button.jsx
+++ client/src/components/UI/Button/Button.jsx
@@ -3,8 +3,16 @@ import styles from './Button.module.css'
 
 const Button = (props) => {
 
+    const variants = [
+        { variant: 'primary', style: { background: '#454ade', color: '#fff', active: '#3d41b5' } },
+        { variant: 'danger', style: { background: '#dc2c32', color: '#fff', active: '#c42e33' } },
+        { variant: 'gray', style: { background: '#cccdd377', color: '#fff', active: '#cccdd399' } },
+    ]
+
+    const variant = variants.find(v => v.variant === props.variant);
+
     return (
-        <button {...props} className={styles.Button}>{props.children}</button>
+        <button {...props} style={variant ? { ...props.style, ...variant.style } : props.style} className={styles.Button}>{props.children}</button>
     )
 }
 

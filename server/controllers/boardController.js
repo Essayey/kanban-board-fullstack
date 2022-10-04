a2e8060d4da7f80f1b0afae8f4b56a9458c40c00
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-04 22:47:57 +0300

    Add invite and kick functionality to board

--- server/controllers/boardController.js
+++ server/controllers/boardController.js
@@ -1,15 +1,18 @@
-const { Board, List, Card } = require('../models/models')
+const { Board } = require('../models/models')
 const { UserBoard } = require('../models/models')
 
 const ApiError = require('../error/ApiError');
 const getBoard = require('./utils');
+const crypto = require("crypto");
 
+const generateToken = () => crypto.randomBytes(30).toString('hex');
 
 class BoardController {
     async create(req, res) {
         const { name, background } = req.body;
-        const board = await Board.create({ name, background: background || '#55f246' });
-        const userBoard = await UserBoard.create({ userId: req.user.id, boardId: board.id })
+
+        const board = await Board.create({ name, background: background || '#55f246', inviteToken: generateToken() });
+        await UserBoard.create({ userId: req.user.id, boardId: board.id })
         return res.json(board);
     }
     async getAll(req, res) {
@@ -20,10 +23,6 @@ class BoardController {
     }
     async getOne(req, res, next) {
         const { id } = req.params;
-        // Find board
-        // This board includes lists array
-        // Lists array include cards array
-        // So, we get board object with all needed information
         const board = await getBoard(id);
 
         if (!board) {
@@ -37,6 +36,57 @@ class BoardController {
 
         res.json(board);
     }
+
+    async generateInviteToken(req, res, next) {
+        const { id } = req.body;
+        const board = await Board.findOne({ where: { id } });
+        if (!board) {
+            return next(ApiError.notFound('Доска не найдена'), req, res);
+        }
+
+        const inviteToken = generateToken();
+        board.set({ inviteToken })
+        await board.save();
+
+        return res.json(inviteToken);
+    }
+    async joinBoard(req, res, next) {
+        const { inviteToken } = req.body;
+        const board = await Board.findOne({ where: { inviteToken } });
+        if (!board) {
+            return next(ApiError.notFound('Доска не найдена или ссылка не действительна'), req, res);
+        }
+        const userBoard = await UserBoard.findOne({ where: { boardId: board.id, userId: req.user.id } });
+        if (userBoard) {
+            return next(ApiError.notFound('Пользователь является участником доски'), req, res);
+        }
+        await UserBoard.create({ boardId: board.id, userId: req.user.id, role: 'Member' });
+
+        return res.json(board.id);
+    }
+
+    async kickMember(req, res, next) {
+        const { boardId, userId } = req.body;
+        const board = await Board.findOne({ where: { id: boardId } });
+
+        if (!board) {
+            return next(ApiError.notFound('Доска не найдена'), req, res);
+        }
+        const user = await UserBoard.findOne({ where: { boardId, userId } })
+        if (!user) {
+            return next(ApiError.badRequest('Такого участника не существует'));
+        }
+
+        const hasAccess = await UserBoard.findOne({ where: { userId: req.user.id, boardId: boardId, role: 'Moderator' } });
+        if (!hasAccess) {
+            return next(ApiError.forbidden('Только модератор может исключать участника'));
+        }
+
+        await user.destroy();
+
+        return res.json(await getBoard(boardId));
+    }
+
     async updateBackground(req, res, next) {
         const { id, value } = req.body;
         const board = await Board.findOne({ where: { id } });
@@ -68,6 +118,8 @@ class BoardController {
 
         return res.json(await getBoard(id));
     }
+
+
 
     // TODO: Add owner role, who can delete board
     // Add invite to the board functionality
--- server/routes/boardRouter.js
+++ server/routes/boardRouter.js
@@ -4,10 +4,17 @@ const boardController = require('../controllers/boardController');
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/', authMiddleware, boardController.create) // Create board
+
 router.get('/', authMiddleware, boardController.getAll) // Get all boards, belongs to user
 router.get('/:id', authMiddleware, boardController.getOne) // Get one board by id
+
+router.put('/generateInviteToken', authMiddleware, boardController.generateInviteToken) // Invite user
 router.put('/background', authMiddleware, boardController.updateBackground) // Update background
 router.put('/name', authMiddleware, boardController.updateName) // Update name
+
+router.put('/joinBoard', authMiddleware, boardController.joinBoard) // joinBoard
+router.put('/kickMember', authMiddleware, boardController.kickMember)
+
 router.delete('/:id', authMiddleware, boardController.delete) // Delete board by id
 
 module.exports = router

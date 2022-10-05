c9fda8fb8a0e188a74e54d58119bd868b14c422d
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-05 18:52:40 +0300

    Create invite component

--- client/src/pages/Invite.jsx
+++ client/src/pages/Invite.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Loader from '../components/UI/Loader/Loader';
+import { boardApi } from '../http/boardAPI';
+import '../Styles/Invite.css'
+import { BOARDS_ROUTE } from '../utils/consts'
 
 const Invite = () => {
+    const [loading, setLoading] = useState(true);
+    const [message, setMessage] = useState('')
+    const { token } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        boardApi.joinBoard(token).then(id => navigate(BOARDS_ROUTE + '/' + id))
+            .catch(err => {
+                setLoading(false);
+                setMessage(err.response.data.message)
+            });
+    }, [])
+
     return (
-        <div>Invite</div>
+        <div className='Invite'>
+            {loading
+                ? <Loader></Loader>
+                : message
+            }
+        </div>
     )
 }
 

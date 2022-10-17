c107b4c9505ce4928c7bd565570c11575681249f
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-17 17:22:39 +0300

    Use StrictMode

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './Styles/App.css';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
-import { useContext, useEffect, useState } from 'react';
+import { StrictMode, useContext, useEffect, useState } from 'react';
 import { check } from './http/userAPI'
 import { observer } from 'mobx-react-lite';
 import { Context } from '.';
@@ -21,10 +21,12 @@ const App = observer(() => {
     if (loading) return;
 
     return (
-        <BrowserRouter>
-            <Navbar />
-            <AppRouter />
-        </BrowserRouter>
+        <StrictMode>
+            <BrowserRouter>
+                <Navbar />
+                <AppRouter />
+            </BrowserRouter>
+        </StrictMode>
     );
 })
 
